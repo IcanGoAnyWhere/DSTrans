2265c1cfbda1e036cc476cb9b2494f66a3abf0ab
xrd <1811485@tongji.edu.cn>
2024-03-10 21:45:20 +0800

    updata ffn

--- pcdet/models/backbones_3d/cross_scale_trans.py
+++ pcdet/models/backbones_3d/cross_scale_trans.py
@@ -25,6 +25,14 @@ class cross_scale_trans(nn.Module):
         self.query_proj = nn.ModuleList()
         self.value_proj = nn.ModuleList()
         self.output_proj = nn.ModuleList()
+        self.dropout = nn.ModuleList()
+        self.norm = nn.ModuleList()
+
+        self.linear1 = nn.ModuleList()
+        self.dropout3 = nn.ModuleList()
+        self.linear2 = nn.ModuleList()
+        self.dropout4 = nn.ModuleList()
+        self.norm3 = nn.ModuleList()
 
         for i in range(self.n_levels):
             self.input_proj.append(nn.Sequential(
@@ -37,6 +45,15 @@ class cross_scale_trans(nn.Module):
             self.query_proj.append(nn.Linear(self.d_model, self.d_chl[i]))
             self.value_proj.append(nn.Linear(self.d_model, self.d_chl[i]))
             self.output_proj.append(nn.Linear(self.d_chl[i], self.d_chl[i]))
+            self.dropout.append(nn.Dropout(model_cfg.Dropout))
+            self.norm.append(nn.LayerNorm(self.d_chl[i]))
+
+            # \\ffn
+            self.linear1.append(nn.Linear(self.d_chl[i], model_cfg.D_ffn))
+            self.dropout3.append(nn.Dropout(model_cfg.Dropout))
+            self.linear2.append(nn.Linear(model_cfg.D_ffn, self.d_chl[i]))
+            self.dropout4.append(nn.Dropout(model_cfg.Dropout))
+            self.norm3.append(nn.LayerNorm(self.d_chl[i]))
 
         # self._reset_parameters()
 
@@ -48,6 +65,12 @@ class cross_scale_trans(nn.Module):
         constant_(self.value_proj.bias.data, 0.)
         xavier_uniform_(self.output_proj.weight.data)
         constant_(self.output_proj.bias.data, 0.)
+
+    def forward_ffn(self, tgt, num):
+        tgt2 = self.linear2[num](self.dropout3[num](F.relu(self.linear1[num](tgt))))
+        tgt = tgt + self.dropout4[num](tgt2)
+        tgt = self.norm3[num](tgt)
+        return tgt
 
     def forward(self, batch_dict):
 
@@ -65,7 +88,7 @@ class cross_scale_trans(nn.Module):
             sampling_offsets = self.sampling_offsets[num](query) \
                 .view(-1, self.n_heads, self.n_points[num], 3)
             # test_2 =self.attention_weights[num](query)
-            attention_weights = torch.softmax(self.attention_weights[num](query)\
+            attention_weights = torch.softmax(self.attention_weights[num](query) \
                 .view(-1, self.n_heads, self.n_points[num]), -1)
 
             # attention_weights = self.attention_weights[num](query) \
@@ -83,27 +106,38 @@ class cross_scale_trans(nn.Module):
             # \\expand value to complete cube with zeros, for meeting grid_sample input
             crt_value_epd = torch.zeros(list(value_epd_shape), device=crt_indice.device)
             crt_indice = crt_indice[:, 1:4]
-            crt_value_epd[:, crt_indice[:, 0].long(), crt_indice[:, 1].long(),
-            crt_indice[:, 2].long()] = value.transpose(0, 1)
+            crt_value_epd[:, crt_indice[:, 0].long(), crt_indice[:, 1].long(),\
+                crt_indice[:, 2].long()] = value.transpose(0, 1)
             crt_value_epd_l = crt_value_epd.unsqueeze(0)
 
+
             # \\norm paras to [0,1]
-            crt_offset = sampling_offsets / crt_normalizer
-            voxel_coods = crt_indice / crt_normalizer
+            crt_offset = sampling_offsets / (crt_normalizer-1)
+            voxel_coods = crt_indice / (crt_normalizer-1)
             sampling_locations = crt_offset + voxel_coods[:, None, None, :]
-            sampling_locations = sampling_locations.unsqueeze(0)
+            swap_matrix = torch.tensor([[0.0, 0.0, 1.0],
+                                        [0.0, 1.0, 0.0],
+                                        [1.0, 0.0, 0.0]],device=value.device)
+            sampling_locations_swap = torch.matmul(sampling_locations, swap_matrix)
+            sampling_locations = sampling_locations_swap.unsqueeze(0)
+
             # \\norm grid to [-1,1]
             sampling_grids = 2 * sampling_locations - 1
             sampling_value_l_ = F.grid_sample(crt_value_epd_l, sampling_grids,
-                                              mode='nearest', padding_mode='zeros', align_corners=False)
+                                              mode='nearest', padding_mode='zeros', align_corners=True)
 
             out = (sampling_value_l_ * attention_weights[None, None, :, :, :]).sum(-1).transpose(1, 2).sum(-1)
+            out = out.reshape(src_shape, self.d_chl[num]).contiguous()
 
-            out = self.output_proj[num](out.reshape(src_shape, self.d_chl[num]).contiguous())
+            out = self.output_proj[num](out)
+            tgt = query + self.dropout[num](out)
+            tgt = self.norm[num](tgt)
+            tgt = self.forward_ffn(tgt, num)
 
-            # out = ms_features[lvl].features + out
 
-            ms_features[lvl] = ms_features[lvl].replace_feature(out)
+            # out = ms_features[lvl].features + tgt
+
+            ms_features[lvl] = ms_features[lvl].replace_feature(tgt)
 
             # ==========debug=================
             # --------------------------------
---
+++ pcdet/models/backbones_3d/cross_scale_trans_test.py
@@ -0,0 +1,73 @@
+from functools import partial
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import math
+import time
+import open3d
+
+# == == == == == debug == == == == == == == == =
+# --------------------------------
+
+
+
+# Create coordinate grids along each dimension
+x = torch.arange(3)-1
+y = torch.arange(3)-1
+z = torch.arange(3)-1
+
+# Use torch.meshgrid to generate coordinate grids
+grid_x, grid_y, grid_z = torch.meshgrid(x, y, z)
+
+# Concatenate the coordinate grids to form a tensor
+cube = torch.stack((grid_x, grid_y, grid_z), dim=-1)
+coordinates = cube.reshape(-1, 3)
+
+cube = cube.to(dtype=torch.float32)
+
+sampling_grids = torch.tensor([2.0, 1.0, 1.0])
+cube_sample = cube[:,2,1,1]
+
+sampling_grids_norm = (2 * sampling_grids / (3-1)) - 1
+
+swap_matrix = torch.tensor([[0.0, 0.0, 1.0],
+                           [0.0, 1.0, 0.0],
+                           [1.0, 0.0, 0.0]])
+
+sampling_grids_norm = torch.matmul(sampling_grids_norm, swap_matrix).view(1,1,1,1,3)
+
+sampling_value_l_ = F.grid_sample(cube.unsqueeze(0), sampling_grids_norm,
+                                  mode='nearest', padding_mode='zeros', align_corners=True)
+
+print("Sample point:", sampling_grids)
+print("Sample result:", sampling_value_l_)
+result_tensor = torch.cat([coordinates, sampling_grids.unsqueeze(0)], dim=0)
+
+pointshow = result_tensor.cpu().numpy()
+point_cloud = open3d.geometry.PointCloud()
+point_cloud.points = open3d.utility.Vector3dVector(pointshow)
+
+# Set colors for each point
+red_color = np.array([1, 0, 0])
+colors = np.tile(red_color, (len(pointshow), 1))
+point_cloud.colors = open3d.utility.Vector3dVector(colors)  # Correct assignment
+
+
+# Set point size
+point_cloud.paint_uniform_color(red_color)  # Use the red_color for uniform coloring
+point_cloud.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+# Set visualization settings with point size
+visualizer = open3d.visualization.Visualizer()
+visualizer.create_window()
+visualizer.add_geometry(point_cloud)
+render_option = visualizer.get_render_option()
+render_option.point_size = 50  # Set the point size here
+
+visualizer.run()
+visualizer.destroy_window()
+
+# --------------------------------
+# == == == == == debug == == == == == == == == =
